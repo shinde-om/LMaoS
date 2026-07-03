6a9373ff03594a8572b8e2d057eeb2123fec2055
Krazy0P <notkanchan@gmail.com>
2026-07-03 23:08:59 +0530

    added google sso + organizations

--- packages/auth/lib/auth.ts
+++ packages/auth/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma"
 import { PrismaClient } from "@workspace/db/generated/prisma/client.js"
 import { PrismaPg } from "@prisma/adapter-pg"
+import { organization } from "better-auth/plugins"
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
@@ -10,10 +11,18 @@ const adapter = new PrismaPg({
 const prisma = new PrismaClient({ adapter })
 
 export const auth = betterAuth({
+  baseURL: process.env.BETTER_AUTH_URL,
   database: prismaAdapter(prisma, {
     provider: "postgresql", // or "mysql", "postgresql", ...etc
   }),
-  emailAndPassword:{
-    enabled: true
-  }
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
+  plugins: [organization()],
 })
